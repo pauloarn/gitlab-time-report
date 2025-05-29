e65ef7cbfb20c485d79d37a39c154f4a0ee0a7ce
Heydrigh <heydrigh@gmail.com>
2025-05-29 13:56:24 -0300

    fix: remove link icon

--- src/components/ui/timelogtable.jsx
+++ src/components/ui/timelogtable.jsx
@@ -44,20 +44,6 @@ export function TimeLogTable({ data, mainKeyInfo, subKey, listOfItems }) {
 											title='Clique para abrir no GitLab'
 										>
 											{item[mainKeyInfo.key]}
-											<svg
-												className='w-4 h-4 inline-block'
-												fill='none'
-												stroke='currentColor'
-												viewBox='0 0 24 24'
-												xmlns='http://www.w3.org/2000/svg'
-											>
-												<path
-													strokeLinecap='round'
-													strokeLinejoin='round'
-													strokeWidth={2}
-													d='M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14'
-												/>
-											</svg>
 										</a>
 									) : (
 										item[mainKeyInfo.key]
